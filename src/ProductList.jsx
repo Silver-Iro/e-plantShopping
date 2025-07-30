46eb14616faef479b63818933f75e5021400845c
Silver-Iro <wadaohe@gmail.com>
2025-07-30 16:28:46 -0400

    added products

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -271,9 +271,27 @@ function ProductList({ onHomeClick }) {
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
+                {console.log(plantsArray)}
             </div>
             {!showCart ? (
                 <div className="product-grid">
+                    <h3>Plant Categories</h3>
+                    {plantsArray.map((plants,index) => (
+                        <div key={index} className="temp">
+                            <ul className="categories">{plants.category}
+                                <li>{plants.plants.map((myplant,index) => (
+                                    <div key={index} className="product-card">
+                                        <h4 className="product-title">{myplant.name}</h4>
+                                        <img className="product-image" src={`${myplant.image}`} alt={myplant.name}/>
+                                        <span className="product-details">{myplant.description}</span>
+                                        <span className="product-price">{ProductList.cost}</span>
+                                        <button className="product-button" onClick="handleAddToCart">Add To Cart</button>
+                                    </div>
+                                ))}</li>
+                            </ul>
+
+                        </div>
+                    )) }
 
 
                 </div>
